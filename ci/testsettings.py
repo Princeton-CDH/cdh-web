5608b29b664b3aa6add6e2fd4a6f13f080525859
Nick Budak <thatbudakguy@gmail.com>
2018-08-31 10:10:34 -0400

    set COMPRESS_ROOT and COMPRESS_OFFLINE

--- ci/testsettings.py
+++ ci/testsettings.py
@@ -1,3 +1,5 @@
+import os
+
 # This file is exec'd from settings.py, so it has access to and can
 # modify all the variables in settings.py.
 
@@ -19,4 +21,10 @@ ALLOWED_HOSTS = ["*"]
 
 # configure django-compressor to compress css & javascript
 COMPRESS_ENABLED = True
+
+# compress to the sitemedia folder
+COMPRESS_ROOT = os.path.join(BASE_DIR, 'sitemedia')
+
+# run a full compress before e2e/a11y tests to serve statically
+COMPRESS_OFFLINE = True
 
